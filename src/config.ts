d91a8addc31374920a22f6ff5ab61e5227ec80d1
kaitlync31 <kaitlynchen2028@u.northwestern.edu>
2025-09-26 11:03:04 -0500

    experience edits

--- src/config.ts
+++ src/config.ts
@@ -54,11 +54,11 @@ export const siteConfig = {
     {
       company: "Northwestern CS Department",
       title: "Undergraduate Teaching Assistant",
-      dateRange: "Aug 2025 - Present",
+      dateRange: "Sep 2025 - Present",
       bullets: [
-        "Supported instruction in CS 211: Fundamentals of Computer Programming in C/C++, guiding students through pointers, memory management, debugging practices, and Git-based workflows",
-        "Held weekly office hours, graded assignments, and provided individualized feedback to 90+ students, helping them strengthen problem-solving skills and coding proficiency",
-        "Collaborated with lead instructor and fellow TAs to develop supplementary materials and improve overall course delivery",
+        "Support instruction in CS 211: Fundamentals of Computer Programming in C/C++, guiding students through pointers, memory management, debugging practices, and Git-based workflows",
+        "Hold weekly office hours, graded assignments, and provided individualized feedback to 90+ students, helping them strengthen problem-solving skills and coding proficiency",
+        "Collaborate with lead instructor and fellow TAs to develop supplementary materials and improve overall course delivery",
       ],
     },
     {
